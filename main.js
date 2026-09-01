bad6eb26ccfbd8cd19f9621a81eb40e0896fa5ad
Nonesaitic <levanthuyet0101@gmail.com>
2026-09-01 15:37:04 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const display = $('.display h3')
+const numbers = $$('.number')
+const operators = $$('.operator')
+const equal = $('#equal')
+const clear = $('#clear')
+const erase = $('#delete')
+let firstNumber = ''
+let secondNumber = ''
+let operator = ''
+let justCalculated = false
+
+// Nhập số
+numbers.forEach(number => {
+  number.addEventListener('click', () => {
+
+    // Vừa tính xong → bấm số mới thì bắt đầu phép tính mới
+    if (justCalculated) {
+      firstNumber = number.textContent
+      secondNumber = ''
+      operator = ''
+      justCalculated = false
+
+      display.textContent = firstNumber
+      return
+    }
+
+    if (operator === '') {
+      firstNumber += number.textContent
+      display.textContent = firstNumber
+    } else {
+      secondNumber += number.textContent
+
+      display.textContent =
+        `${firstNumber} ${operator} ${secondNumber}`
+    }
+
+  })
+})
+
+// Chọn phép tính
+operators.forEach(button => {
+  button.addEventListener('click', () => {
+    operator = button.textContent
+
+    display.textContent = `${firstNumber} ${operator}`
+  })
+})
+// Bấm =
+equal.addEventListener('click', () => {
+
+  const a = Number(firstNumber)
+  const b = Number(secondNumber)
+
+  let result
+
+  if (operator === '+') {
+    result = a + b
+  }
+
+  if (operator === '-') {
+    result = a - b
+  }
+
+  if (operator === '×') {
+    result = a * b
+  }
+
+  if (operator === '÷') {
+    result = a / b
+  }
+
+  display.textContent = result
+
+  firstNumber = String(result)
+  secondNumber = ''
+  operator = ''
+
+  justCalculated = true
+})
+
+// C
+clear.addEventListener('click', () => {
+  firstNumber = ''
+  secondNumber = ''
+  operator = ''
+  display.textContent = ''
+})
+
+// delete
+
+erase.addEventListener('click', () => {
+
+  if (operator !== '' && secondNumber === '') {
+    operator = ''
+    display.textContent = firstNumber
+    return
+  }
+
+  if (operator === '') {
+    firstNumber = firstNumber.slice(0, -1)
+    display.textContent = firstNumber || ''
+  } else {
+    secondNumber = secondNumber.slice(0, -1)
+    display.textContent =
+      `${firstNumber} ${operator} ${secondNumber}`
+  }
+
+})
